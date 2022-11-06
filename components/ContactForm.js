ed32f90faababe5ebaca97fd21a62bf453196762
Kyle-Ski <Kyle.Czajkowski.Ski@gmail.com>
2022-11-05 19:05:27 -0600

    removing form POST functionality

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -14,42 +14,8 @@ const ContactForm = () => {
       }
     }
     Object.assign(data, injectedData)
-
-    if (data.name?.includes('http') || data.email?.includes('http') || data.address?.includes('http') || data.message?.includes('http')) {
-      setStatus('We appreciate your message!')
-      return
-    }
-    
-    fetch(CONTACT_ENDPOINT, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        // Check for likely spam/bot request, validate via captcha
-        if (response.status === 422) {
-          Object.keys(injectedData).forEach((key) => {
-            const el = document.createElement('input')
-            el.type = 'hidden'
-            el.name = key
-            el.value = injectedData[key]
-            e.target.appendChild(el)
-          })
-
-          e.target.submit()
-          throw new Error('Please finish the captcha challenge')
-        }
-
-        if (response.status !== 200) {
-          throw new Error(response.statusText)
-        }
-        return response.json()
-      })
-      .then(() => setStatus('We appreciate your message!'))
-      .catch((err) => setStatus(err.toString()))
+    setStatus('We appreciate your message!')
+    return
   }
 
   if (status) {
